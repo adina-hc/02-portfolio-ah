5664bfd5b84c09ed389228ca6f7d1b845292af10
Adina Henderson <adinahenderson.mail@gmail.com>
2021-05-07 19:08:25 -0400

    updated styling, added pdf to resume section, cleaned up html

---
+++ develop/script.js
@@ -0,0 +1,5 @@
+// Initialize Materialize
+M.AutoInit();
+
+var codingAssessment = "https://adina-hc.github.io/04-coding-assessment/";
+var sportsAlmanac = "https://adina-hc.github.io/sports-almanac/";
