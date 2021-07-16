e3950387dfbc9e7c0cc5cfa817e12527585292e9
Adina Henderson <adinahenderson.mail@gmail.com>
2021-07-16 15:54:50 -0400

    added image for latest project, modified script and index documents

--- develop/script.js
+++ develop/script.js
@@ -1,5 +1,32 @@
 // Initialize Materialize
 M.AutoInit();
 
-var codingAssessment = "https://adina-hc.github.io/04-coding-assessment/";
-var sportsAlmanac = "https://adina-hc.github.io/sports-almanac/";
+// Defining variables for the links
+const hPsShtr = "https://desolate-tundra-25750.herokuapp.com/";
+const sAlmnc = "https://adina-hc.github.io/sports-almanac/";
+const wApp = "https://adina-hc.github.io/06-weather-dashboard/";
+const cTst = "https://adina-hc.github.io/04-coding-assessment/";
+
+// Query selector's for event listeners
+const happyPetsEl = document.getElementbyID("happyPets");
+const sportsAlmanacEl = document.getElementById("sportsAlmanac");
+const weatherAppEl = document.getElementById("weatherApp");
+const codingTestEl = document.getElementById("codingTest");
+
+// Create event listeners for each image to go to the website
+
+happyPetsEl.addEventListener("click", () => {
+  window.open(hPsShtr, "_blank", "height=800px, width=1000px");
+});
+
+sportsAlmanacEl.addEventListener("click", () => {
+  window.open(sAlmnc, "_blank", "height=800px, width=1000px");
+});
+
+weatherAppEl.addEventListener("click", () => {
+  window.open(wApp, "_blank", "height=800px, width=1000px");
+});
+
+codingTestEl.addEventListener("click", () => {
+  window.open(cTst, "_blank", "height=800px, width=1000px");
+});
